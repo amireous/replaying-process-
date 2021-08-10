648e13b44ddde584722fdfec0d245d0b5122334b
Amir SF <82820601+amireous@users.noreply.github.com>
2021-08-10 19:01:40 +0430

    add item element

---
+++ script.js
@@ -0,0 +1,24 @@
+const mainAddBtn = document.querySelector(".add-btn");
+const mainRemoveBtn = document.querySelector(".remove-all-btn");
+
+let itemID = 0;
+
+function addItem(parent) {
+  itemID++;
+
+  const item = document.createElement("div");
+
+  item.classList.add("item");
+  item.id = itemID;
+
+  // 3- set a text and buttons for item EL
+  item.innerHTML = `
+  <div class = "item-el ">
+   <p class = "item-text" >div id : <span>${item.id}</span> </p>
+   <button class="add-child-btn">add child</button>
+   <button class="remove-child-btn">remove child</button>
+   </div>`;
+
+  // - append item EL as parent child
+  parent.appendChild(item);
+}
